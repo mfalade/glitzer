a9d3d57d0739d21f980bacd760901e64109ee53a
Mayowa Falade <mayowa.falade@ithaka.org>
2017-04-28 22:27:03 +0100

    update setup

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ setup(
   name = 'glitzer',
   packages = ['glitzer'],
   version = '0.1',
-  description = 'Lay emphasis on desired strings by adding pretty and customizable borders',
+  description = 'Lay emphasis on desired string by adding pretty and customizable borders',
   author = 'Mayowa Falade',
   author_email = 'falademayowa240@gmail.com',
   url = 'https://github.com/mfalade/glitzer',
-  download_url = 'https://github.com/mfalade/glitzer/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/mfalade/glitzer/archive/0.1.tar.gz',
   keywords = ['border', 'print', 'printing', 'log', 'pretty'],
   classifiers = [],
 )
